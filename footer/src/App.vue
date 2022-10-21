a1ac6d02c4a9f18c2d4be271903b6af38db66192
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-10-21 19:18:27 -0300

    chore: create vue3 footer app

---
+++ footer/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <p>Micro frontends footer</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+footer {
+  background-color: #1876d1;
+  color: white;
+  font-size: 1rem;
+  width: 100%;
+  padding: 8px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
